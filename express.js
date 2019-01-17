5c4aa70a3572548709f3b2f781ac6c8dce74cb75
Jamila Toaha <jtoahacoding@gmail.com>
2019-01-17 16:29:25 -0500

    latest commit

--- express.js
+++ express.js
@@ -11,4 +11,26 @@ app.use(morgan("dev"));
 const staticMiddleware = express.static(__dirname + "public");
 app.use(staticMiddleware);
 
-app.use(bodyParser.urlencode({ extend: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get("/", (req, res, next) => {
+   res.send("Hello World");
+});
+
+const PORT = 1377;
+
+app.listen(PORT, () => {
+  console.log( `App listening in port ${PORT}`)
+});
+
+
+//hi june this is what I installed:
+// 1) npm install express
+// 2) Install nodemon with npm install nodemon --save-dev
+// 3) Now open your project folder and change or add the scripts.start key in package.json to say nodemon instead of node, like this:
+
+// "scripts": {
+//   "start": "nodemon ./path/to/your/app.js" <<add this line to the scripts section
+// },  and that's as far as I got so far
