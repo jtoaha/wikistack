425a8bdebd2791ed3bbda35587e9db685fe4b695
June Hosoya <junehosoya@gmail.com>
2019-01-17 15:42:18 -0500

    setting up the first page

--- express.js
+++ express.js
@@ -1,1 +1,14 @@
-const express = require('express');
+//express has its own set of functions
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+//calling express/invoking express() is creating app that can listen/use middleware CRUD comes with incoking express()
+const app = express();
+//express.Router and express.static
+
+app.use(morgan("dev"));
+
+const staticMiddleware = express.static(__dirname + "public");
+app.use(staticMiddleware);
+
+app.use(bodyParser.urlencode({ extend: true }));
